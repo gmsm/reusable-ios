38d9aa0ef496dc086beda089c00bca519e9dad35
Glauco Moraes <glaucomoraes@outlook.com>
2018-08-29 13:55:29 -0300

    Create UITableViewExtensions

---
+++ Reusable/Reusable/Util/Extensions/UITableViewExtensions.swift
@@ -0,0 +1,29 @@
+//
+//  UITableViewExtensions.swift
+//  Reusable
+//
+//  Created by Glauco Moraes on 29/08/18.
+//  Copyright © 2018 Glauco Moraes. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Unable to Dequeue Reusable Table View Cell - \(T.reuseIdentifier)")
+        }
+        return cell
+    }
+    
+    func registerFromCodeView<T: UITableViewCell>(_: T.Type) {
+        register(T.self, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func register<T: UITableViewCell>(_: T.Type) {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        register(nib, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+}
